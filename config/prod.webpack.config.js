e993c731c985f8fc149d314a68240359eb4908d0
Josh Kinlaw <jkinlaw@redhat.com>
2018-09-20 11:44:25 -0400

    fixing stable releases

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -1,9 +1,9 @@
 /* global require, module */
 
 const _ = require('lodash');
-const config = _.cloneDeep(require('./base.webpack.config'));
+const webpackConfig = require('./base.webpack.config');
 
 module.exports = _.merge({},
-    config,
-    require('./prod.webpack.plugins.js')
+    webpackConfig,
+    require('./base.webpack.plugins.js')
 );
